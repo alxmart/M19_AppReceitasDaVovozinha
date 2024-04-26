2c3442bb1ae42199381529cb147cf9e41e8b74f2
alxmart <alxmartinez@gmail.com>
2024-04-26 15:29:30 -0300

    Setting var for RecyclerView, and defining layout for RV

--- app/src/main/java/com/luizafmartinez/m19_appreceitasdavovozinha/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m19_appreceitasdavovozinha/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerReceitas: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerReceitas = findViewById(R.id.rvReceitas)
+        //recyclerReceitas.adapter =
+        recyclerReceitas.layoutManager = LinearLayoutManager(this)
+
+
     }
 }
